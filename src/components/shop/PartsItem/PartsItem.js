81e509b0ace990ec9a6f43c7d92394c7c23a4570
Melvin <chen.melv@husky.neu.edu>
2019-10-09 16:17:10 -0400

    parts shop

---
+++ src/components/shop/PartsItem/PartsItem.js
@@ -0,0 +1,108 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+
+import "./PartsItem.scss";
+import { CardContent } from "@material-ui/core";
+import { Card } from "@material-ui/core";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Slide from "@material-ui/core/Slide";
+import Button from "@material-ui/core/Button";
+
+//Transition for dialog on button click
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+// Parts Shop Container
+export default class PartsItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isModalOpen: false
+    };
+  }
+
+  toggleModalState = setToBool => {
+    this.setState({
+      isModalOpen: setToBool
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Card
+          className={"shop-item"}
+          onClick={() => this.toggleModalState(true)}
+        >
+          <CardContent>
+            <Grid container direction="column" alignItems="center" spacing={2}>
+              <Grid item>
+                <img
+                  src={"/assets/purple_hex.png"}
+                  alt={this.props.name}
+                  className={"item-mainimg"}
+                />
+              </Grid>
+              <Grid item>
+                <Typography variant="h6">{this.props.name}</Typography>
+              </Grid>
+              <Grid item>
+                <div className={"item-price"}>
+                  <img src={"/assets/purple_hex.png"} />
+                  <Typography varient="body2">{this.props.price}</Typography>
+                </div>
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
+
+        <Dialog
+        className={"shop-item-dialog"}
+          open={this.state.isModalOpen}
+          TransitionComponent={Transition}
+          keepMounted
+          onClose={() => this.toggleModalState(false)}
+          aria-labelledby="alert-dialog-slide-title"
+          aria-describedby="alert-dialog-slide-description"
+        >
+          <DialogTitle id="alert-dialog-slide-title">
+            {this.props.name}
+          </DialogTitle>
+          <DialogContent>
+            <Grid container direction="column" alignItems="center" spacing={2}>
+              <Grid item>
+                <img src={"/assets/purple_hex.png"} />
+              </Grid>
+              <Grid item>
+                <div className={"item-price"}>
+                  <img src={"/assets/purple_hex.png"} />
+                  <Typography varient="body2">{this.props.price}</Typography>
+                </div>
+              </Grid>
+            </Grid>
+          </DialogContent>
+          <DialogActions>
+            <Button
+              onClick={() => this.toggleModalState(false)}
+              color="primary"
+            >
+              Cancel
+            </Button>
+            <Button
+              onClick={() => this.toggleModalState(false)}
+              color="primary"
+            >
+              Buy
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  }
+}
--- src/components/shop/PartsShop.js
+++ src/components/shop/PartsShop.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 import Navbar from "../navbar/Navbar";
+import PartsItem from "./PartsItem/PartsItem";
 
 import "./PartsShop.scss";
 
@@ -22,6 +24,51 @@ export default class PartsShop extends React.Component {
         <Grid container direction={"column"} className={"content"} spacing={6}>
           <Grid item>
             <h1>Parts Shop</h1>
+          </Grid>
+          <Grid
+            item
+            container
+            direction={"row"}
+            className={"listing"}
+            spacing={4}
+          >
+            <Grid item xs={3}>
+              <PartsItem
+                img={`testimg`}
+                name={`testname`}
+                price={`testprice`}
+              />
+            </Grid>
+            <Grid item xs={3}>
+              <PartsItem
+                img={`testimg`}
+                name={`testname`}
+                price={`testprice`}
+              />
+            </Grid>
+            <Grid item xs={3}>
+              <PartsItem
+                img={`testimg`}
+                name={`testname`}
+                price={`testprice`}
+              />
+            </Grid>
+
+            <Grid item xs={3}>
+              <PartsItem
+                img={`testimg`}
+                name={`testname`}
+                price={`testprice`}
+              />
+            </Grid>
+
+            <Grid item xs={3}>
+              <PartsItem
+                img={`testimg`}
+                name={`testname`}
+                price={`testprice`}
+              />
+            </Grid>
           </Grid>
         </Grid>
       </div>
